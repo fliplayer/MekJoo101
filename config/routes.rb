50cec88bff2eb516f37d6ee7c23a9e7d50b9ef0d
Han Lee <fliplayer@gmail.com>
2015-09-16 20:24:34 -0700

    Changed Production configs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
   mount Ckeditor::Engine => '/ckeditor'
-  devise_for :users, path_names: {
-  		sign_up: ''
-	}
+  devise_for :users
   resources :posts do
   	resources :comments
   end
